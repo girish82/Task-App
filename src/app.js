7f3512ae0934d8238501484c070d42a9f09aee7c
girish82 <pgirish4u@gmail.com>
2020-04-08 12:59:36 +0200

    env done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const userRouter = require('../src/routers/user')
 const taskRouter = require('../src/routers/task')
 require('../src/db/mongoose')
 
+app.use(cors())
+
 app.use(express.json())
 
 // const PORT = process.env.PORT 
