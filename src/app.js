e0fc5e1fd3c6b06b27c39e75a468b1afecb8376f
girish82 <pgirish4u@gmail.com>
2020-04-08 10:16:46 +0200

    env done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@ require('../src/db/mongoose')
 app.use(express.json())
 
 // const PORT = process.env.PORT 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE");
+    res.header("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(userRouter)
 app.use(taskRouter)
