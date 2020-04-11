1b7530d486f380dd82c59f583a0ecbeb07e01790
girish82 <pgirish4u@gmail.com>
2020-04-11 11:44:16 +0200

    body parser

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 
 // const PORT = process.env.PORT 
